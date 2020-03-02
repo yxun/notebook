5f9b3e78941320e809a561ebc9b717891b973945
yxun <yuanlin.yxu@gmail.com>
2020-03-01 21:16:31 -0500

    Add creational patterns builder examples

---
+++ design/patterns/builder/Hero.java
@@ -0,0 +1,84 @@
+import java.io.WriteAbortedException;
+
+/**
+ * Imagine a character generator for a role playing game. 
+ * The easiest option is to let computer create the character for you. 
+ * But if you want to select the character details like profession, gender, hair color etc. 
+ * the character generation becomes a step-by-step process that completes 
+ * when all the selections are ready.
+ */
+
+public final class Hero {
+    private final Profession profession;
+    private final String name;
+    private final HairType hairType;
+    private final HairColor hairColor;
+    private final Armor armor;
+    private final Weapon weapon;
+
+    private Hero(Builder builder) {
+        this.profession = builder.profession;
+        this.name = builder.name;
+        this.hairColor = builder.hairColor;
+        this.hairType = builder.hairType;
+        this.weapon = builder.weapon;
+        this.armor = builder.armor;
+    }
+
+
+    public static class Builder {
+        private final Profession profession;
+        private final String name;
+        private HairType hairType;
+        private HairColor hairColor;
+        private Armor armor;
+        private Weapon weapon;
+
+        public Builder(Profession profession, String name) {
+            if (profession == null || name == null) {
+                throw new IllegalArgumentException("profession and name can not be null");
+            }
+            this.profession = profession;
+            this.name = name;
+        }
+
+        public Builder withHairType(HairType hairType) {
+            this.hairType = hairType;
+            return this;
+        }
+
+        public Builder withHairColor(HairColor hairColor) {
+            this.hairColor = hairColor;
+            return this;
+        }
+
+        public Builder withArmor(Armor armor) {
+            this.armor = armor;
+            return this;
+        }
+
+        public Builder withWeapon(Weapon weapon) {
+            this.weapon = weapon;
+            return this;
+        }
+
+        public Hero build() {
+            return new Hero(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        var mage = new Hero.Builder(Profession.MAGE, "Riobard")
+            .withHairColor(HairColor.BLACK)
+            .withWeapon(Weapon.DAGGER)
+            .build();
+    }
+
+
+    /** Reference
+     * https://java-design-patterns.com/patterns/builder/
+     * Design Patterns: Elements of Reusable Object-Oriented Software
+     * Effective Java (2nd Edition)
+     */  
+}
+
---
+++ design/patterns/builder/User.java
@@ -0,0 +1,149 @@
+
+
+// Builder pattern aims to 
+// “Separate the construction of a complex object from its representation 
+// so that the same construction process can create different representations.”
+
+public class User {
+
+    // All final attributes
+    private final String firstName; // required
+    private final String lastName;  // required
+    private final int age;          // optional
+    private final String phone;     // optional
+    private final String address;   // optional
+
+    private User(UserBuilder builder) {
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.phone = builder.phone;
+        this.address = builder.address;
+    }
+
+    // All getter, and No setter to provide immutability
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public String toString() {
+        return "User: " +
+                this.firstName + ", " +
+                this.lastName + ", " +
+                this.age + ", " +
+                this.phone + "," +
+                this.address;
+    }
+
+
+    public static class UserBuilder {
+        private final String firstName;
+        private final String lastName;
+        private int age;
+        private String phone;
+        private String address;
+
+        public UserBuilder(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public UserBuilder age(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public UserBuilder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public UserBuilder address(String address) {
+            this.address = address;
+            return this;
+        }
+
+        // Return the finally constructed User object
+        public User build() {
+            User user = new User(this);
+            validateUserObject(user);
+            return user;
+        }
+
+        private void validateUserObject(User user) {
+            // Do some basic validations to check
+            // if user object does not break any assumption
+
+        }
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User.UserBuilder("Lokesh", "Gupta")
+            .age(30)
+            .phone("123456")
+            .address("fake address 123")
+            .build();
+        System.out.println(user1);
+
+        User user2 = new User.UserBuilder("Jack", "Reacher")
+            .age(40)
+            .phone("2345")
+            // no address
+            .build();
+        System.out.println(user2);
+
+        User user3 = new User.UserBuilder("Super", "Man")
+            .build();
+        System.out.println(user3);
+
+    }
+
+    /** Existing implementations in JDK
+     * java.lang.StringBuilder#append()
+     * java.lang.StringBuffer#append()
+     * java.nio.ByteBuffer#put()
+     * javax.swing.GroupLayout.Group#addComponent()
+     */
+
+    /** Advantages
+     * design flexibility
+     * more readable code
+     * reduced parameters to the constructor
+     * highly readable method calls
+     * no need to pass in null for optional parameters
+     * help build immutable objects
+     */
+
+    /** Disadvantages
+     * double up total lines of code
+     * client is more verbose
+     */
+
+    /** References
+     * http://en.wikipedia.org/wiki/Builder_pattern
+     * http://www.javaspecialists.eu/archive/Issue163.html
+     * http://en.wikipedia.org/wiki/Fluent_interface
+     * http://martinfowler.com/bliki/FluentInterface.html
+     */
+
+}
+
+
+
